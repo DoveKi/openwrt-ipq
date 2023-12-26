8adf4f1ac8b0c9ed3010c9e1d95a30536f368e6f
doveki <2290218900@qq.com>
2023-12-27 02:30:29 +0800

    ipq807x: don't build usb driver for ax6/ax3600

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -215,7 +215,7 @@ define Device/redmi_ax6
 	$(call Device/xiaomi_ax3600)
 	DEVICE_VENDOR := Redmi
 	DEVICE_MODEL := AX6
-	DEVICE_PACKAGES := ipq-wifi-redmi_ax6
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 -kmod-usb3 -kmod-usb-dwc3 -kmod-usb-dwc3-qcom
 endef
 TARGET_DEVICES += redmi_ax6
 
@@ -237,7 +237,7 @@ define Device/xiaomi_ax3600
 	DEVICE_DTS_CONFIG := config@ac04
 	SOC := ipq8071
 	KERNEL_SIZE := 36608k
-	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax3600
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax3600 -kmod-usb3 -kmod-usb-dwc3 -kmod-usb-dwc3-qcom
 ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),)
 	ARTIFACTS := initramfs-factory.ubi
 	ARTIFACT/initramfs-factory.ubi := append-image-stage initramfs-uImage.itb | ubinize-kernel
